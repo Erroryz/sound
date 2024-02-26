c7f3afa9ae0d94c64613640f420537759ba1f51f
Error <129003249+Erroryz@users.noreply.github.com>
2024-02-26 23:59:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('tempo-restante').style.opacity = '0'; // Oculta o texto inicialmente
+});
+function fadeInTempo() {
+    var tempoRestante = document.getElementById('tempo-restante');
+    if (tempoRestante.style.opacity !== '1') {
+        var opacidadeAtual = parseFloat(tempoRestante.style.opacity);
+        var novaOpacidade = opacidadeAtual + 0.1;
+        tempoRestante.style.opacity = novaOpacidade;
+        setTimeout(fadeInTempo, 100); // Chama a função novamente após 100ms para um efeito suave
+    }
+}
+function tocarMusica() {
+    var musica = document.getElementById("musica");
+    musica.play();
+    musica.addEventListener('timeupdate', function() {
+        var tempoAtual = musica.currentTime;
+        var duracaoTotal = musica.duration;
+        var tempoRestante = duracaoTotal - tempoAtual;
+        document.getElementById('tempo-restante').textContent =  formatarTempo(tempoRestante);
+        fadeInTempo()
+    });
+}
+
+function formatarTempo(segundos) {
+    var minutos = Math.floor(segundos / 60);
+    var segundosFormatados = Math.floor(segundos % 60);
+    segundosFormatados = segundosFormatados < 10 ? '0' + segundosFormatados : segundosFormatados;
+    return minutos + ':' + segundosFormatados;
+}
+
